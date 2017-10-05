b3183f1b5c24762a6e05b357e0dfa0dcdd42f92f
quang.td95 <quang.td95@gmail.com>
2017-10-05 23:21:03 +0700

    first commit

---
+++ app/src/main/java/com/quangtd/photoeditor/presenter/PresenterBase.java
@@ -0,0 +1,8 @@
+package com.quangtd.photoeditor.presenter;
+
+/**
+ * QuangTD on 10/5/2017.
+ */
+
+public abstract class PresenterBase {
+}
---
+++ app/src/main/java/com/quangtd/photoeditor/view/adapter/ViewHolderBase.java
@@ -0,0 +1,8 @@
+package com.quangtd.photoeditor.view.adapter;
+
+/**
+ * QuangTD on 10/5/2017.
+ */
+
+public class ViewHolderBase {
+}
